326dbcd2ed3e7fc52d8dc2bbb90ceb3efadebcbc
Gabriel Villagrán <44887537+Villagran29A@users.noreply.github.com>
2021-04-15 23:11:37 -0500

    Add files via upload

---
+++ Edit_VillagranSaucedoCerdaEstrada/Editor Imágenes/Form1.Designer.cs
@@ -0,0 +1,162 @@
+﻿namespace Editor_Imágenes
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.archivoToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.abrirToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.guardarToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.pictureBox = new System.Windows.Forms.PictureBox();
+            this.filtrosToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.negativoToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.blancoYNegroToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.grisesToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.sepíaToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.archivoToolStripMenuItem,
+            this.filtrosToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(950, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // archivoToolStripMenuItem
+            // 
+            this.archivoToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.abrirToolStripMenuItem,
+            this.guardarToolStripMenuItem});
+            this.archivoToolStripMenuItem.Name = "archivoToolStripMenuItem";
+            this.archivoToolStripMenuItem.Size = new System.Drawing.Size(60, 20);
+            this.archivoToolStripMenuItem.Text = "Archivo";
+            // 
+            // abrirToolStripMenuItem
+            // 
+            this.abrirToolStripMenuItem.Name = "abrirToolStripMenuItem";
+            this.abrirToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.abrirToolStripMenuItem.Text = "Abrir";
+            this.abrirToolStripMenuItem.Click += new System.EventHandler(this.abrirToolStripMenuItem_Click);
+            // 
+            // guardarToolStripMenuItem
+            // 
+            this.guardarToolStripMenuItem.Name = "guardarToolStripMenuItem";
+            this.guardarToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.guardarToolStripMenuItem.Text = "Guardar";
+            this.guardarToolStripMenuItem.Click += new System.EventHandler(this.guardarToolStripMenuItem_Click);
+            // 
+            // pictureBox
+            // 
+            this.pictureBox.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pictureBox.Location = new System.Drawing.Point(0, 24);
+            this.pictureBox.Name = "pictureBox";
+            this.pictureBox.Size = new System.Drawing.Size(950, 509);
+            this.pictureBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.pictureBox.TabIndex = 1;
+            this.pictureBox.TabStop = false;
+            // 
+            // filtrosToolStripMenuItem
+            // 
+            this.filtrosToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.negativoToolStripMenuItem,
+            this.blancoYNegroToolStripMenuItem,
+            this.grisesToolStripMenuItem,
+            this.sepíaToolStripMenuItem});
+            this.filtrosToolStripMenuItem.Name = "filtrosToolStripMenuItem";
+            this.filtrosToolStripMenuItem.Size = new System.Drawing.Size(51, 20);
+            this.filtrosToolStripMenuItem.Text = "Filtros";
+            // 
+            // negativoToolStripMenuItem
+            // 
+            this.negativoToolStripMenuItem.Name = "negativoToolStripMenuItem";
+            this.negativoToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.negativoToolStripMenuItem.Text = "Negativo";
+            this.negativoToolStripMenuItem.Click += new System.EventHandler(this.negativoToolStripMenuItem_Click);
+            // 
+            // blancoYNegroToolStripMenuItem
+            // 
+            this.blancoYNegroToolStripMenuItem.Name = "blancoYNegroToolStripMenuItem";
+            this.blancoYNegroToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.blancoYNegroToolStripMenuItem.Text = "Blanco y negro";
+            this.blancoYNegroToolStripMenuItem.Click += new System.EventHandler(this.blancoYNegroToolStripMenuItem_Click);
+            // 
+            // grisesToolStripMenuItem
+            // 
+            this.grisesToolStripMenuItem.Name = "grisesToolStripMenuItem";
+            this.grisesToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.grisesToolStripMenuItem.Text = "Grises";
+            this.grisesToolStripMenuItem.Click += new System.EventHandler(this.grisesToolStripMenuItem_Click);
+            // 
+            // sepíaToolStripMenuItem
+            // 
+            this.sepíaToolStripMenuItem.Name = "sepíaToolStripMenuItem";
+            this.sepíaToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.sepíaToolStripMenuItem.Text = "Sepía";
+            this.sepíaToolStripMenuItem.Click += new System.EventHandler(this.sepíaToolStripMenuItem_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.ClientSize = new System.Drawing.Size(950, 533);
+            this.Controls.Add(this.pictureBox);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem archivoToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem abrirToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem guardarToolStripMenuItem;
+        private System.Windows.Forms.PictureBox pictureBox;
+        private System.Windows.Forms.ToolStripMenuItem filtrosToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem negativoToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem blancoYNegroToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem grisesToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem sepíaToolStripMenuItem;
+    }
+}
+
---
+++ Edit_VillagranSaucedoCerdaEstrada/Editor Imágenes/Form1.cs
@@ -0,0 +1,190 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Editor_Imágenes
+{
+
+
+    public partial class Form1 : Form
+    {
+        Bitmap bmp;
+        string imagen = null;
+        bool isFile = false;
+        bool fileSaved = false;
+
+
+        public Form1()
+        {
+            InitializeComponent();
+
+        }
+
+
+        private void abrirToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog d = new OpenFileDialog();
+            d.Filter = "Imágenes (*.jpg)|*.jpg|Imágenes (*.png)|*.png";
+            DialogResult r = d.ShowDialog();
+            if(r == DialogResult.OK)
+            {
+                bmp = (Bitmap)Image.FromFile(d.FileName);
+                imagen = d.FileName;
+                pictureBox.Image = bmp;
+                isFile = true;
+            }
+
+        }
+
+        private void guardar()
+        {
+            if(isFile)
+            {
+                SaveFileDialog d = new SaveFileDialog();
+                d.Filter = "Imágenes (*.jpg)|*.jpg|Imágenes (*.png)|*.png";
+                DialogResult r = d.ShowDialog();
+                if(r == DialogResult.OK)
+                {
+                    bmp.Save(d.FileName);
+                    fileSaved = true;
+                }
+            }
+        }
+
+        private void guardarToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            guardar();
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {   
+            if(!fileSaved && isFile)
+            {
+                DialogResult r = MessageBox.Show("Salir sin guardar","Cerrar",MessageBoxButtons.YesNo);
+                if(r == DialogResult.Yes)
+                {
+                    Application.Exit();
+                }
+                else if(r == DialogResult.No)
+                {
+                    guardar();
+                }
+            }
+        }
+
+        private void negativoToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            bmp = (Bitmap)Image.FromFile(imagen);
+            for (int h = 0; h < bmp.Height; h++)
+            {
+                for(int w = 0; w < bmp.Width; w++)
+                {
+                    Color c = bmp.GetPixel(w, h);
+                    Color n = Color.FromArgb(c.A, 255-c.R, 255-c.G, 255-c.B);
+                    bmp.SetPixel(w, h, n);
+                }
+            }
+            pictureBox.Image = bmp;
+            fileSaved = false;
+        }
+
+        private void blancoYNegroToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            int avg;
+            bmp = (Bitmap)Image.FromFile(imagen);
+            for (int h = 0; h < bmp.Height; h++)
+            {
+                for (int w = 0; w < bmp.Width; w++)
+                {
+                    Color c = bmp.GetPixel(w, h);
+                    avg = (c.R + c.G + c.B) / 3;
+                    Color n;
+                    if(avg>=127)
+                    {
+                        n = Color.Black;
+                    }
+                    else
+                    {
+                        n = Color.White;
+                    }
+                    bmp.SetPixel(w, h, n);
+                }
+            }
+            pictureBox.Image = bmp;
+            fileSaved = false;
+        }
+
+        private void grisesToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            int avg;
+            bmp = (Bitmap)Image.FromFile(imagen);
+            for (int h = 0; h < bmp.Height; h++)
+            {
+                for (int w = 0; w < bmp.Width; w++)
+                {
+                    Color c = bmp.GetPixel(w, h);
+                    avg = (c.R + c.G + c.B) / 3;
+                    Color n = Color.FromArgb(c.A, avg, avg, avg);
+                    bmp.SetPixel(w, h, n);
+                }
+            }
+            pictureBox.Image = bmp;
+            fileSaved = false;
+        }
+
+        private void sepíaToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            int tr, tg, tb;
+            int r, g, b;
+            bmp = (Bitmap)Image.FromFile(imagen);
+            for (int h = 0; h < bmp.Height; h++)
+            {
+                for (int w = 0; w < bmp.Width; w++)
+                {
+                    Color c = bmp.GetPixel(w, h);
+                    r = c.R;
+                    g = c.G;
+                    b = c.B;
+                    tr = (int)(0.393 * c.R + 0.769 * c.G + 0.189 * c.B);
+                    tg = (int)(0.349 * c.R + 0.686 * c.G + 0.168 * c.B);
+                    tb = (int)(0.272 * c.R + 0.534 * c.G + 0.131 * c.B);
+                    if(tr > 255)
+                    {
+                        r = 255;
+                    }
+                    else
+                    {
+                        r = tr;
+                    }
+                    if (tg > 255)
+                    {
+                        g = 255;
+                    }
+                    else
+                    {
+                        r = tg;
+                    }
+                    if (tb > 255)
+                    {
+                        b = 255;
+                    }
+                    else
+                    {
+                        r = tb;
+                    }
+                    Color n = Color.FromArgb(c.A, r, g, b);
+                    bmp.SetPixel(w, h, n);
+                }
+            }
+            pictureBox.Image = bmp;
+            fileSaved = false;
+
+        }
+    }
+}
